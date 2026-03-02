bb9e2cf86cbda19ef28769f280d0b5f6156929f4
Alokkumar1411 <2k24.cs1c.2414120@gmail.com>
2026-03-02 18:58:05 +0530

    change

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-var tl=gsap.timeline()
-var menu=document.querySelector("#nav i")
-var cross=document.querySelector("#full i")
-tl.to("#full",{
-    right:0,
-    duration:0.5,
-})
-tl.from("#full h4",{
-    x:150,
-    stagger:0.26,
-    duration:0.4,
-    opacity:0,
+var tl = gsap.timeline()
 
+var menu = document.querySelector("#nav i")
+var cross = document.querySelector("#full i")
+
+tl.to("#full",{
+    x: "-100%",
+    duration: 0.5
 })
+
+tl.from("#full h4",{
+    x: 150,
+    stagger: 0.2,
+    opacity: 0,
+    duration: 0.4
+})
+
 tl.from("#full i",{
-    opacity:0,
-    duration:0.6,
+    opacity: 0,
+    duration: 0.3
 })
 
 tl.pause()
@@ -22,6 +25,7 @@ tl.pause()
 menu.addEventListener("click",function(){
     tl.play()
 })
+
 cross.addEventListener("click",function(){
     tl.reverse()
 })
