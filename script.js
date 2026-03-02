a634adaa164d25905e2523b8a2c2a1805744a187
Alokkumar1411 <2k24.cs1c.2414120@gmail.com>
2026-03-02 17:59:17 +0530

    gsap-practice

---
+++ script.js
@@ -0,0 +1,27 @@
+var tl=gsap.timeline()
+var menu=document.querySelector("#nav i")
+var cross=document.querySelector("#full i")
+tl.to("#full",{
+    right:0,
+    duration:0.5,
+})
+tl.from("#full h4",{
+    x:150,
+    stagger:0.26,
+    duration:0.4,
+    opacity:0,
+
+})
+tl.from("#full i",{
+    opacity:0,
+    duration:0.6,
+})
+
+tl.pause()
+
+menu.addEventListener("click",function(){
+    tl.play()
+})
+cross.addEventListener("click",function(){
+    tl.reverse()
+})
